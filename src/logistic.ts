ef229d8e7220c44d12063cf7f112e2f0f7e62b85
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-10-15 18:32:24 +0100

    Logistic function

---
+++ src/logistic.ts
@@ -0,0 +1,11 @@
+const r = 0.5;
+const K = 100;
+
+export function rhs(t: number, y: number[], dydt: number[]) {
+    const n = y[0];
+    dydt[0] = r * (n - n / K);
+}
+
+export function initial() {
+    return [1];
+}
