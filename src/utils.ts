8aefd897aeadeaa9aa9e3c70df34c6c4a114c457
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-10-19 08:30:46 +0100

    Csv output

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
+import * as fs from "fs";
+
 export function square(x: number) : number{
     return x * x;
 }
 
 
-export function constrain(x: number, min: number, max: number) : number{
+export function constrain(x: number, min: number, max: number) : number {
     return Math.max(Math.min(x, max), min);
 }
 
@@ -14,3 +16,22 @@ export function copy_array(to: number[], from: number[]) : void {
         to[i] = from[i];
     }
 }
+
+
+export function zeros(n: number) : number[] {
+    const ret = Array<number>(n);
+    for (var i = 0; i < n; ++i) {
+        ret[i] = 0.0;
+    }
+    return ret;
+}
+
+
+// need to install node types for this to work...
+export function write_csv(arr: Array<number[]>, path: string) : void {
+    var str = arr.map(x => x.join(',') + '\n').join('');
+    fs.writeFile(path, str, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    });}
